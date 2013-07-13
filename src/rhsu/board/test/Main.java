bfbc9e440f50ad41453ee391504b25379f89c24e
robert <rhsu123@gmail.com>
2013-07-13 10:06:55 -0400

    #41: more issues

--- src/rhsu/board/test/Main.java
+++ src/rhsu/board/test/Main.java
@@ -11,28 +11,16 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		/*Matrix b = new DoubleBoard(2,2);
-		b.setTypeAt(0, 1, 5.6);
-		b.setTypeAt(1, 0, 5.8);
-		
-		Matrix a = new DoubleBoard(2,2);
-		a.setTypeAt(0, 1, 50.0);
-		a.setTypeAt(1, 0, 50.0);
-		
-		System.out.println(b);
-		System.out.println(a);
-		
-		Matrix c = a.Add(b);
-		
-		System.out.println(c);*/
-		
 		BoardReader reader = new BoardReader();
 		reader.mainPrompt();
-		
-		AbstractBoard b = new IntegerBoard(1,1);
-
-		Board<CharPiece> test = b.ConvertFromStringBoard(reader.getOutputBoard());
+		StringBoard test = reader.getOutputBoard();
 		
 		System.out.println(test);
+		
+		IntegerBoard b = new IntegerBoard(1,1);
+		
+		b = (IntegerBoard) b.ConvertFromStringBoard(test);
+		
+		System.out.println(b);
 	}
 }
