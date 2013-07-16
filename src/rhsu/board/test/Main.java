d25e9d2335e9a85ac75ad2a02806871be00fc543
rhsu <rhsu123@gmail.com>
2013-07-15 21:50:12 -0400

    #35: think I got it

--- src/rhsu/board/test/Main.java
+++ src/rhsu/board/test/Main.java
@@ -11,15 +11,15 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		DoubleBoard b = new DoubleBoard(1,1);
+		//DoubleBoard b = new DoubleBoard(1,1);
 		
-		double d1 = (double) b.getTypeAt(0, 0);
+		//double d1 = (double) b.getTypeAt(0, 0);
 		
-		double d2 = (double) b.pieceAt(0, 0).getType();
+		//double d2 = (double) b.pieceAt(0, 0).getType();
 		
-		DoublePiece db = new DoublePiece(1,1,1);
+		DoublePiece test = new DoublePiece(1,1,1);
 		
-		Double d3 = (Double) db.getType();
+		Double d3 = (Double) test.getType();
 		
 		System.out.println(d3);
 	}
