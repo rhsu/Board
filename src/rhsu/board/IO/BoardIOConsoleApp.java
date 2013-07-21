252d83804b2ec1f07fb8712bf9885f9a370eda19
rhsu <rhsu123@gmail.com>
2013-07-21 10:36:12 -0400

    cleanup

--- src/rhsu/board/IO/BoardIOConsoleApp.java
+++ src/rhsu/board/IO/BoardIOConsoleApp.java
@@ -1,11 +1,8 @@
 package rhsu.board.IO;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 import rhsu.board.implementations.StringBoard;
 import rhsu.board.utilities.UtilityFunctions;
 
