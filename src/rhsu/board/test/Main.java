361b70819a7a9b49e85679be9a54c12eac734aa8
rhsu <rhsu123@gmail.com>
2013-08-02 09:52:37 -0400

    #55: removed reflection board

--- src/rhsu/board/test/Main.java
+++ src/rhsu/board/test/Main.java
@@ -10,20 +10,11 @@ import rhsu.board.IO.*;
 import rhsu.board.arithmetic.Matrix;
 import rhsu.board.implementations.math.*;
 import rhsu.board.implementations.*;
-import rhsu.board.reflection.ReflectionBoard;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		/*CharBoard b = new CharBoard("test.txt");
-		
-		System.out.println(b);*/
-		
-		ReflectionBoard<String> myStuff = new ReflectionBoard<>(String.class, 10, 10);
-		
-		myStuff.setTypeAt(0, 0, "abcdefg");
-		
-		System.out.println(myStuff.getTypeAt(0, 0));
+
 	}
 }
