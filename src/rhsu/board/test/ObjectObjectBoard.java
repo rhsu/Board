2c61807167ffaf05c70ffa3f5b01615f0ebdcce4
rhsu <rhsu123@gmail.com>
2013-08-07 09:14:04 -0400

    #57: Begin Object Object Board

---
+++ src/rhsu/board/test/ObjectObjectBoard.java
@@ -0,0 +1,91 @@
+package rhsu.board.test;
+
+import rhsu.board.Board;
+import rhsu.board.BoardPiece;
+
+/**
+ *
+ * @author rhsu
+ */
+public class ObjectObjectBoard implements Board
+{
+
+	@Override
+	public BoardPiece pieceAt(int i, int j) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public BoardPiece getLeftPiece(int i, int j) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public BoardPiece getLeftPiece(BoardPiece p) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public BoardPiece getRightPiece(int i, int j) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public BoardPiece getRightPiece(BoardPiece p) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public BoardPiece getDownPiece(int i, int j) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public BoardPiece getDownPiece(BoardPiece p) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public BoardPiece getUpPiece(int i, int j) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public BoardPiece getUpPiece(BoardPiece p) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public Object getTypeAt(int i, int j) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public void setTypeAt(int i, int j, Object t) 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public int getHorizontal_size() 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public int getVertical_size() 
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
