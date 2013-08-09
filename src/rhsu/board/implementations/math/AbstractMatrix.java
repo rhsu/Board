325623b7c094b5751f5f56c3bb32b82b560e93f3
rhsu <rhsu123@gmail.com>
2013-08-09 12:25:53 -0400

    #36 begin creation of abstract matrix

---
+++ src/rhsu/board/implementations/math/AbstractMatrix.java
@@ -0,0 +1,15 @@
+package rhsu.board.implementations.math;
+
+import rhsu.board.AbstractBoard;
+
+/**
+ *
+ * @author rhsu
+ */
+public class AbstractMatrix<T> extends AbstractBoard<T>
+{
+	public AbstractMatrix(int h, int v)
+	{
+		super(h,v);
+	}
+}
