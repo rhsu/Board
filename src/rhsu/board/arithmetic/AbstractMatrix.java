39d02be0fef0c2e9b84ca16db71cb3fd31230a80
rhsu <rhsu123@gmail.com>
2013-08-30 10:09:14 -0400

    Created new check function

--- src/rhsu/board/arithmetic/AbstractMatrix.java
+++ src/rhsu/board/arithmetic/AbstractMatrix.java
@@ -8,6 +8,15 @@ import rhsu.board.AbstractBoard;
  */
 public abstract class AbstractMatrix<T> extends AbstractBoard<T> implements Matrix<T>
 {
+	protected enum OperationType
+	{
+		ADD,
+		SUBTRACT,
+		MULTIPLY,
+		DETERMINANT,
+		INVERSE
+	}
+	
 	public AbstractMatrix(int h, int v)
 	{
 		super(h, v);
@@ -18,7 +27,38 @@ public abstract class AbstractMatrix<T> extends AbstractBoard<T> implements Matr
 		super(filename);
 	}
 	
-	protected void CheckAddDimensions(Matrix m)
+	protected void Check(OperationType type)
+	{
+		Check(type, null);
+	}
+	
+	protected void Check(OperationType type, Matrix<T> otherMatrix)
+	{
+		switch(type)
+		{
+			case ADD:
+			case SUBTRACT:
+				if(otherMatrix.getHorizontal_size() != this.getHorizontal_size()
+					|| otherMatrix.getVertical_size() != this.getVertical_size())
+				{
+					throw new ArrayIndexOutOfBoundsException("The dimensions do no match");
+				}
+			case MULTIPLY:
+				if(this.getHorizontal_size() != otherMatrix.getVertical_size()
+					|| this.getVertical_size() != otherMatrix.getHorizontal_size())
+				{
+					throw new ArrayIndexOutOfBoundsException("The dimensions do no match");
+				}
+			case DETERMINANT:
+			case INVERSE:
+				if(this.getVertical_size() != this.getHorizontal_size())
+				{
+					throw new ArrayIndexOutOfBoundsException("The dimensions do not match");
+				}
+		}
+	}
+	
+	protected void CheckAddDimensions(Matrix<T> m)
 	{
 		if(m.getHorizontal_size() != this.getHorizontal_size() 
 			|| m.getVertical_size() != this.getVertical_size())
@@ -27,7 +67,7 @@ public abstract class AbstractMatrix<T> extends AbstractBoard<T> implements Matr
 		}
 	}
 	
-	protected void CheckMultiplyDimensions(Matrix m)
+	protected void CheckMultiplyDimensions(Matrix<T> m)
 	{
 		if(this.getHorizontal_size() != m.getVertical_size()
 			|| this.getVertical_size() != m.getHorizontal_size())
