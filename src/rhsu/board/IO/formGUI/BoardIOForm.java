354d6df6b8c26c787067821a4e879fa6a53975f1
robert <rhsu123@gmail.com>
2013-09-15 10:22:44 -0400

    #37: fixed up formatting

--- src/rhsu/board/IO/formGUI/BoardIOForm.java
+++ src/rhsu/board/IO/formGUI/BoardIOForm.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package rhsu.board.IO.formGUI;
 
 /**
@@ -13,7 +9,8 @@ public class BoardIOForm extends javax.swing.JFrame {
 	/**
 	 * Creates new form NewJFrame
 	 */
-	public BoardIOForm() {
+	public BoardIOForm() 
+	{
 		initComponents();
 	}
 
@@ -46,7 +43,8 @@ public class BoardIOForm extends javax.swing.JFrame {
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String args[]) {
+	public static void main(String args[]) 
+	{
 		/* Set the Nimbus look and feel */
 		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
