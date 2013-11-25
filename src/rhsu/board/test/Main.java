6b0ffe2f10feaaf99d378db6b178b65812c0595d
rhsu <rhsu123@gmail.com>
2013-11-25 13:49:23 -0500

    committing for bacup

--- src/rhsu/board/test/Main.java
+++ src/rhsu/board/test/Main.java
@@ -14,44 +14,9 @@ public class Main
 	{
 		System.out.println(o);
 	}
-		
-	public static long main1()
-	{		
-		long startTime = System.currentTimeMillis();
-		Iterator<BoardPiece<Integer>> iter = test.iterBoard();
-		
-		while(iter.hasNext())
-		{
-			BoardPiece<Integer> item = (BoardPiece<Integer>) iter.next();
-			print(item);
-		}
-		long endTime = System.currentTimeMillis();
-		long duration = endTime - startTime;
-		
-		return duration;
-	}
-	
-	public static long main2()
-	{
-		long startTime = System.currentTimeMillis();
-		for(int i = 0; i < test.getHorizontal_size(); i++)
-		{
-			for(int j = 0; j< test.getVertical_size(); j++)
-			{
-				print(test.pieceAt(i, j));
-			}
-		}
-		
-		long endTime = System.currentTimeMillis();
-		long duration = endTime - startTime;
-		
-		return duration;
-	}
-	
-	static IntegerBoard test = new IntegerBoard(100,100, 1);
 	
 	public static void main(String[] args)
 	{	
-		BenchmarkTest.Test();
+
 	}
 }
