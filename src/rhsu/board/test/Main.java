10ce581bf462ff85835aec54a2616ac955cd3d54
rhsu <rhsu123@gmail.com>
2013-12-02 17:08:43 -0500

    #90: Seems to work. will leave open

--- src/rhsu/board/test/Main.java
+++ src/rhsu/board/test/Main.java
@@ -20,6 +20,8 @@ public class Main
 	
 	public static void main(String[] args)
 	{	
-		IntegerBoard board = new IntegerBoard(ResourceRetriever.GetResource("ExampleIntegerBoard"));
+		//IntegerBoard board = new IntegerBoard(ResourceRetriever.GetResource("ExampleIntegerBoard"));
+		IntegerBoard board = new IntegerBoard("test.txt");
+		print(board);
 	}
 }
