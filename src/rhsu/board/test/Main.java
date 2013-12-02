75782e12d7b7cedb9a8d5f28041d8d163dc61ea8
rhsu <rhsu123@gmail.com>
2013-12-02 14:55:31 -0500

    #31: I think this might work!

--- src/rhsu/board/test/Main.java
+++ src/rhsu/board/test/Main.java
@@ -1,8 +1,16 @@
 package rhsu.board.test;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.net.URL;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import rhsu.board.*;
 import rhsu.board.IO.*;
 import rhsu.board.implementations.*;
@@ -10,6 +18,22 @@ import rhsu.board.implementations.arithmetic.*;
 
 public class Main
 {
+	public Main()
+	{
+		try 
+		{	
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader( getClass().getResourceAsStream(
+					"/rhsu/board/resources/resource.txt")));
+			
+			
+		} 
+		catch (Exception ex) 
+		{
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
+	}
+	
 	public static void print(Object o)
 	{
 		System.out.println(o);
@@ -17,6 +41,7 @@ public class Main
 	
 	public static void main(String[] args)
 	{	
-
+		Main myProj = new Main();
+		
 	}
 }
