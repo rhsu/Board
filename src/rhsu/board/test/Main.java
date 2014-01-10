90938f4c5ef304176b5f318abec148e8f058b7b5
rhsu <rhsu123@gmail.com>
2014-01-10 14:34:57 -0500

    committing nonsense for backup

--- src/rhsu/board/test/Main.java
+++ src/rhsu/board/test/Main.java
@@ -10,6 +10,7 @@ import rhsu.board.*;
 import rhsu.board.IO.*;
 import rhsu.board.exceptionHandler.ExceptionHandler;
 import rhsu.board.resources.ResourceRetriever;
+import rhsu.board.utilities.PrintUtilityDebugger;
 import rhsu.board.xc.*;
 
 public class Main
@@ -19,17 +20,54 @@ public class Main
 		System.out.println(o);
 	}
 	
+	private int a;
+	
+	public Main(int a)
+	{
+		this.a = a;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return a + "";
+	}
+	
 	public static void main(String[] args)
 	{	
-		StringBoard board = new StringBoard(2,2);
-		StringBoard board3 = new StringBoard(2,2);
-		CharBoard board2 = new CharBoard(2,2);
+		PrintUtilityDebugger debugger = new PrintUtilityDebugger();
 		
-		//print(board.getClass());
-		//print(board2.getClass());
+		//testing swap
 		
-		//print(board.getClass().equals(board2.getClass()));
+		/*int a = 1;
+		int b = 10;
 		
-		print(board.equals(board2));
+		print("a is: " + a);
+		print("b is: " + b);
+		
+		int temp;
+		temp = a;
+		a = b;
+		b = temp;
+		
+		print("a is: " + a);
+		print("b is: " + b);*/
+		
+		//testing object swap
+		
+		Main x = new Main(1);
+		Main y = new Main(10);
+		
+		print("x is: " + x);
+		print("y is: " + y);
+		
+		Main temp;
+		
+		temp = x;
+		x = y;
+		y = temp;
+		
+		print("x is: " + x);
+		print("y is: " + y);
 	}
 }
