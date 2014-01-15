c7612e978e8c9f5b2f440bae000cc69a73f8d0ae
rhsu <rhsu123@gmail.com>
2014-01-15 15:14:54 -0500

    #109: fixed up main

--- src/rhsu/board/test/Main.java
+++ src/rhsu/board/test/Main.java
@@ -23,10 +23,6 @@ public class Main
 	
 	public static void main(String[] args)
 	{	
-		AbstractBasicBoard<Integer> board = new IntegerBoard(5,5,0);
 		
-		BoardPiece<Integer>[][] test = board.getBoard();
-		
-		//test[0][0] = 99;
 	}
 }
