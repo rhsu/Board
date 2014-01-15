4a97a95d8516ed5a3e6f08c1d177b3b2230bcb23
rhsu <rhsu123@gmail.com>
2014-01-15 15:41:46 -0500

    #108: fixes

--- src/rhsu/board/test/Main.java
+++ src/rhsu/board/test/Main.java
@@ -29,6 +29,19 @@ public class Main
 	
 	public static void main(String[] args)
 	{	
-
+		MobilityBoard<Integer> board1 = new BasicMobilityBoard(5,5, 0);
+		MobilityBoard<Integer> board2 = new BasicMobilityBoard(5,5, 0);
+		
+		board1.setValueAt(0,0, -99);
+		
+		
+		print(board1);
+		print("-----");
+		
+		board1.move((MobilityPiece<Integer>) board1.pieceAt(0,0), 1, 1, board2);
+		
+		print(board1);
+		print("-----");
+		print(board2);
 	}
 }
