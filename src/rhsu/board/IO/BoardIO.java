ed8e22346b3ce5920bdd2d6c7d29a7ab27dc8a79
rhsu <rhsu123@gmail.com>
2014-01-27 13:08:48 -0500

    114: added in BoardIO interface

--- src/rhsu/board/IO/BoardIO.java
+++ src/rhsu/board/IO/BoardIO.java
@@ -1,6 +1,9 @@
 package rhsu.board.IO;
 
+import java.io.BufferedReader;
+
 public interface BoardIO 
 {
-	
+	public void populateFromFile(String filename);
+	public void populateFromResource(BufferedReader reader);
 }
