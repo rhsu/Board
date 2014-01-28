750b14e5edec3269a335e3b486bc425663720674
rhsu <rhsu123@gmail.com>
2014-01-28 16:11:17 -0500

    #118: created new properties for AbstractBasicMatrix

--- src/rhsu/board/basic/AbstractBasicMatrix.java
+++ src/rhsu/board/basic/AbstractBasicMatrix.java
@@ -3,13 +3,16 @@ package rhsu.board.basic;
 import java.io.BufferedReader;
 import rhsu.board.Matrix;
 import rhsu.board.exceptionHandler.ExceptionHandler;
+import rhsu.board.exceptionHandler.HandleType;
 
 /**
  *
  * @author rhsu
  */
-public abstract class AbstractBasicMatrix<T> extends AbstractBasicBoard<T> implements Matrix<T>
+public abstract class AbstractBasicMatrix<T> 
+	extends AbstractBasicBoard<T> implements Matrix<T>
 {
+
 	protected enum OperationType
 	{
 		ADD,
@@ -18,14 +21,20 @@ public abstract class AbstractBasicMatrix<T> extends AbstractBasicBoard<T> imple
 		SQUAREMATRIX
 	}
 	
+	//<editor-fold desc="Properties" defaultstate="collapsed">
+
 	protected ExceptionHandler<T> handler;
-		
+	protected HandleType handleType;
+	protected T defaultValue;
+	
+	//</editor-fold>
+	
 	public AbstractBasicMatrix(int horizontal, int vertical, T defaultValue)
 	{
 		super(horizontal, vertical, defaultValue);
 	}
 			
-	/*public AbstractBasicMatrix(String filename)
+	public AbstractBasicMatrix(String filename)
 	{
 		super(filename);
 		handler = new ExceptionHandler<>();
@@ -33,8 +42,9 @@ public abstract class AbstractBasicMatrix<T> extends AbstractBasicBoard<T> imple
 	
 	public AbstractBasicMatrix(BufferedReader bufferedReader)
 	{
-		super(bufferedReader);
-	}*/
+		super(bufferedReader);		
+		handler = new ExceptionHandler<>();
+	}
 	
 	protected void CheckDimensions(OperationType type)
 	{
