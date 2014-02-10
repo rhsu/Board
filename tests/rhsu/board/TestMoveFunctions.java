b3b9943ef5d36c6e90ba327774184c938ede454e
rhsu <rhsu123@gmail.com>
2014-02-10 16:06:55 -0500

    #112: redoing the unit tests

---
+++ tests/rhsu/board/TestMoveFunctions.java
@@ -0,0 +1,20 @@
+package rhsu.board;
+
+import org.junit.*;
+
+/**
+ *
+ * @author rhsu
+ */
+public class TestMoveFunctions 
+{
+	private static Board<Integer> mockBoard;
+	private static BoardPiece<Integer> mockPieceZero;
+	
+	private static BoardPiece<Integer> mockPieceDestination;
+	
+	private static Board<Integer> otherBoard;
+	private static BoardPiece<Integer> otherBoardPiece;
+	
+	private static final String TEST_RESOURCE = "/rhsu/board/resources/test/";
+}
