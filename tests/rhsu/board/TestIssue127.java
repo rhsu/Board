645314de18b93551ad1c1da975647917600722a3
robert <rhsu123@gmail.com>
2014-02-16 10:55:02 -0500

    #127: added in unit test for getting the piece in all directions

--- tests/rhsu/board/TestIssue127.java
+++ tests/rhsu/board/TestIssue127.java
@@ -15,17 +15,16 @@ public class TestIssue127
 		Board<Integer> test = new IntegerBoard(
 			ResourceRetriever.GetResource("Test127", TEST_RESOURCE));
 		
-		System.out.println(test);
-		
-		BoardPiece<Integer> testPiece = test.getPieceAt(2, 2);
-		
 		//UP
-		System.out.println(testPiece);
+		assertEquals((int)test.getValueAt(2, 2, Direction.UP, 0), 1);
 		
 		//DOWN
+		assertEquals((int)test.getValueAt(2, 2, Direction.DOWN, 0), 4);
 		
 		//LEFT
+		assertEquals((int)test.getValueAt(2, 2, Direction.LEFT, 0), 2);
 		
 		//RIGHT
+		assertEquals((int)test.getValueAt(2, 2, Direction.RIGHT, 0), 3);
 	}
 }
