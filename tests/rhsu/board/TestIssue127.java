26b2badcfb878d7b83c69ff0ff6d76c386aa1294
robert <rhsu123@gmail.com>
2014-02-16 09:43:10 -0500

    127: committing for backup

--- tests/rhsu/board/TestIssue127.java
+++ tests/rhsu/board/TestIssue127.java
@@ -2,13 +2,25 @@ package rhsu.board;
 
 import static org.junit.Assert.*;
 import org.junit.*;
+import rhsu.board.basic.implementations.arithmetic.IntegerBoard;
+import rhsu.board.resources.ResourceRetriever;
 
 public class TestIssue127 
 {
+	private static final String TEST_RESOURCE = "/rhsu/board/resources/test/";
+	
 	@Test
 	public void test_GetValueAt()
 	{
+		Board<Integer> test = new IntegerBoard(
+			ResourceRetriever.GetResource("Test127", TEST_RESOURCE));
+		
+		System.out.println(test);
+		
+		BoardPiece<Integer> testPiece = test.getPieceAt(2, 2);
+		
 		//UP
+		System.out.println(testPiece);
 		
 		//DOWN
 		
