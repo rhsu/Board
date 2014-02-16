e854c63e81f000bc433e827a66cd6cec51fc0322
robert <rhsu123@gmail.com>
2014-02-15 20:59:10 -0500

    #127: Begin unit test

---
+++ tests/rhsu/board/TestIssue127.java
@@ -0,0 +1,19 @@
+package rhsu.board;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestIssue127 
+{
+	@Test
+	public void test_GetValueAt()
+	{
+		//UP
+		
+		//DOWN
+		
+		//LEFT
+		
+		//RIGHT
+	}
+}
