b1f6bed5f7a9f718c5332c8ab88e5247fc9b3831
rhsu <rhsu123@gmail.com>
2014-04-02 14:29:56 -0400

    #129: simplified the unit test

--- tests/rhsu/board/MobilityTest.java
+++ tests/rhsu/board/MobilityTest.java
@@ -3,18 +3,11 @@ package rhsu.board;
 import org.junit.*;
 import static org.junit.Assert.*;
 import rhsu.board.basic.implementations.arithmetic.IntegerBoard;
-import rhsu.board.resources.ResourceRetriever;
-import rhsu.board.test.MockFactory;
 
 public class MobilityTest 
-{
-	private static final String TEST_RESOURCE = "/rhsu/board/resources/test/";
-	
-	private static final Board<Integer> source = new IntegerBoard(
-		ResourceRetriever.GetResource("testMoveBoardSource", TEST_RESOURCE));
-		
-	private static final Board<Integer> destination = new IntegerBoard(
-		ResourceRetriever.GetResource("testMoveBoardDestination",TEST_RESOURCE));
+{	
+	private static final Board<Integer> source = new IntegerBoard(3,3,1);
+	private static final Board<Integer> destination = new IntegerBoard(3,3,0);
 	
 	@Test
 	public void test()
