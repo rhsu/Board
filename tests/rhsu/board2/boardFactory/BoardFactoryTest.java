4b8a13dc4e17db5f8ae8b416e6ecfdffd6c97f60
rhsu <rhsu123@gmail.com>
2014-05-15 15:53:44 -0400

    added in unit test for board factory

---
+++ tests/rhsu/board2/boardFactory/BoardFactoryTest.java
@@ -0,0 +1,41 @@
+package rhsu.board2.boardFactory;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class BoardFactoryTest
+{
+	private BoardFactory boardFactory;
+	private final int horizontalSize = 100;
+	private final int verticalSize = 200;
+	
+	@Before
+	public void setupBoardFactory()
+	{
+		boardFactory = new BoardFactory(horizontalSize, verticalSize);
+	}
+	
+	@Test
+	public void testGetHorizontalSize()
+	{
+		assert(boardFactory.getHorizontalSize() == horizontalSize);
+	}
+	
+	@Test
+	public void testGetVerticalSize()
+	{
+		assert(boardFactory.getVerticalSize() == verticalSize);
+	}
+	
+	@Test
+	public void testCreateBoard2ImplReturnsNotNull()
+	{
+		assertNotNull(boardFactory.createBoard2Impl());
+	}
+	
+	@Test
+	public void testCreateIntegerBoardReturnsNotNull()
+	{
+		assertNotNull(boardFactory.createIntegerBoard());
+	}
+}
