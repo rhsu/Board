bc69459371c07e5e342ebcd488df618d08c2b3b4
rhsu <rhsu123@gmail.com>
2014-05-16 10:51:17 -0400

    #156: Finished boardpiece test working on impl test

--- tests/rhsu/board2/BoardPiece2Test.java
+++ tests/rhsu/board2/BoardPiece2Test.java
@@ -1,11 +1,17 @@
-package rhsu.board2;
+package rhsu.board2;;
 
 import org.junit.*;
+import static org.junit.Assert.*;
+import rhsu.board2.factory.BoardPieceFactory;
 
 @Ignore
 public class BoardPiece2Test
 {
+	protected static final Object VALUE_TO_SET = "VALUE";
+	protected static final int INDEX_TO_SET = 1500;
+	
 	protected BoardPiece2 boardPiece;
+	protected BoardPieceFactory factory;
 	
 	@Before
 	public void setupBoardPiece()
@@ -16,27 +22,44 @@ public class BoardPiece2Test
 	@Test
 	public void testSetValue()
 	{
-		
+		boardPiece.setValue(VALUE_TO_SET);
+		assertEquals(VALUE_TO_SET, boardPiece.getValue());
 	}
 	
 	@Test
 	public void testGetValue()
 	{
-		
+		assertEquals(factory.getBoardPieceValue(), boardPiece.getValue());
 	}
 	
 	@Test
-	public void testGetHorizontal()
+	public void testSetHorizontalIndex()
 	{
-		
+		//TODO: Negative index should throw exception
+		boardPiece.setHorizontalIndex(INDEX_TO_SET);
+		assertEquals(INDEX_TO_SET, boardPiece.getHorizontalIndex());
 	}
 	
 	@Test
-	public void testGetVertical()
+	public void testGetHorizontalIndex()
 	{
-		
+		assertEquals(factory.getHorizontalIndex(), boardPiece.getHorizontalIndex());
 	}
 	
+	@Test
+	public void testSetVerticalIndex()
+	{
+		boardPiece.setVerticalIndex(INDEX_TO_SET);
+		assertEquals(INDEX_TO_SET, boardPiece.getVerticalIndex());
+	}
+	
+	@Test
+	public void testGetVerticalIndex()
+	{
+		assertEquals(factory.getHorizontalIndex(), boardPiece.getHorizontalIndex());
+	}
+	
+	/*TODO: these will be implemented once mobility is done
 	@Test
 	public void testGetMobilityStatus()
 	{
@@ -59,5 +82,5 @@ public class BoardPiece2Test
 	public void testsetUUID()
 	{
 		
-	}
+	}*/
 }
