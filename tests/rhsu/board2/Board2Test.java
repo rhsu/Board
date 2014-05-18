bc0c3cd17da32793dacc63646c8350558b5a1803
robert <rhsu123@gmail.com>
2014-05-18 08:45:57 -0400

    #163: implemented but have some new questions

--- tests/rhsu/board2/Board2Test.java
+++ tests/rhsu/board2/Board2Test.java
@@ -1,6 +1,7 @@
 package rhsu.board2;
 
 import org.junit.*;
+import static org.junit.Assert.*;
 import rhsu.board2.factory.BoardFactory;
 
 @Ignore
@@ -39,5 +40,39 @@ public class Board2Test
 	public void testGetDefaultValue()
 	{
 		assert(board.getDefaultValue() == null);
+	}
+	
+	@Test
+	public void testEqualsSelf()
+	{
+		assertEquals(board, board);
+	}
+	
+	
+	/**
+	 * Tests that the same hash code is generated if the two boards are equals
+	 */
+	@Test
+	public void testEqualsSelf_SameHashCode()
+	{
+		assertEquals(board.hashCode(), board.hashCode() );
+	}
+	
+	@Test
+	public void testDoesNotEqualDifferentBoard()
+	{
+		//TODO: need to expand the board factory to do some new stuff
+	}
+	
+	@Test
+	public void testEqualsSame()
+	{
+		//TODO: Factory with Generics
+	}
+	
+	@Test
+	public void testEqualsSame_SameHashCode()
+	{
+		
 	}
 }
