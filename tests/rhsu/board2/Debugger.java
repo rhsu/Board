876f5b4c0aae0b136f3ae6a63d1e448dcadc961d
rhsu <rhsu123@gmail.com>
2014-06-13 11:17:52 -0400

    closes #173

--- tests/rhsu/board2/Debugger.java
+++ tests/rhsu/board2/Debugger.java
@@ -1,6 +1,7 @@
 package rhsu.board2;
 
 import rhsu.board2.basic.BoardPieceImpl;
+import rhsu.board2.basic.implementations.arithmetic.DoubleBoard2;
 import rhsu.board2.basic.implementations.arithmetic.IntegerBoard2;
 import rhsu.board2.factory.BoardFactory;
 
@@ -8,12 +9,12 @@ public class Debugger
 {
 	public static void main(String[] args)
 	{
-		BoardFactory<IntegerBoard2> factory = BoardFactory.createFactory(IntegerBoard2.class, 5, 5);
-		IntegerBoard2 result = factory.createBoard();
+		BoardFactory<DoubleBoard2> factory = BoardFactory.createFactory(DoubleBoard2.class, 5, 5);
+		DoubleBoard2 result = factory.createBoard();
 		
-		for (BoardPieceImpl<Integer>[] boardArray : result.getBoardArray())
+		for (BoardPieceImpl<Double>[] boardArray : result.getBoardArray())
 		{
-			for (BoardPieceImpl<Integer> boardPiece : boardArray)
+			for (BoardPieceImpl<Double> boardPiece : boardArray)
 			{
 				System.out.println(boardPiece.getValue());
 			}
