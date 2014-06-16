c4bdc9373323f2e7ab68fbb92e1cbcb42552b95e
rhsu <rhsu123@gmail.com>
2014-06-16 16:47:27 -0400

    #181: fixes an issue with comparing doubles

--- src/rhsu/board2/basic/implementations/arithmetic/DoubleBoard2.java
+++ src/rhsu/board2/basic/implementations/arithmetic/DoubleBoard2.java
@@ -1,6 +1,7 @@
 package rhsu.board2.basic.implementations.arithmetic;
 
 import rhsu.board2.basic.BoardImpl;
+import rhsu.board2.basic.BoardPieceImpl;
 
 public class DoubleBoard2 extends BoardImpl<Double>
 {
@@ -25,10 +26,34 @@ public class DoubleBoard2 extends BoardImpl<Double>
 		
 		BoardImpl instance = (BoardImpl) aInstance;
 		
+		Object test = instance.getBoardArray()[0][0].getValue();
+		
+		if (!(test instanceof Double)) return false;
+		
 		return
 			instance.getHorizontalSize() == this.getHorizontalSize() &&
 			instance.getVerticalSize() == this.getVerticalSize() &&
-			instance.toString().equals(this.toString());
+			//instance.toString().equals(this.toString());
+			EqualHelper(instance.getBoardArray());
+	}
+	
+	public boolean EqualHelper(BoardPieceImpl<Double>[][] otherBoardArray)
+	{	
+		for (int i = 0; i < this.getVerticalSize(); i++)
+		{
+			for (int j = 0; j < this.getHorizontalSize(); j++)
+			{	
+				Double otherValue = otherBoardArray[i][j].getValue();
+				Double thisValue = this.getBoardArray()[i][j].getValue();
+				
+				if (otherValue.compareTo(thisValue) != 0)
+				{
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 
 	@Override
