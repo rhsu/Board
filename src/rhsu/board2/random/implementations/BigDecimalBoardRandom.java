e0ddd9421028f245137cfa67fca8edccd0dccc14
rhsu <rhsu123@gmail.com>
2014-06-20 10:38:25 -0400

    #158: big decimal board

--- src/rhsu/board2/random/implementations/BigDecimalBoardRandom.java
+++ src/rhsu/board2/random/implementations/BigDecimalBoardRandom.java
@@ -1,9 +1,14 @@
 package rhsu.board2.random.implementations;
 
 import java.math.BigDecimal;
+import java.util.Random;
 import rhsu.board2.implementations.BigDecimalBoard2;
+import rhsu.board2.random.RandomBoard;
+import rhsu.board2.random.RandomGenerator2;
 
-public class BigDecimalBoardRandom extends BigDecimalBoard2 
+public class BigDecimalBoardRandom 
+	extends BigDecimalBoard2 
+	implements RandomBoard<BigDecimal>
 {
 	public BigDecimalBoardRandom(int horizontalSize, int verticalSize, BigDecimal defaultValue)
 	{
@@ -13,5 +18,21 @@ public class BigDecimalBoardRandom extends BigDecimalBoard2
 	public BigDecimalBoardRandom(int horizontalSize, int verticalSize)
 	{
 		super(horizontalSize, verticalSize);
+	}
+
+	@Override
+	public RandomGenerator2<BigDecimal> randomGenerator()
+	{
+		RandomGenerator2<BigDecimal> generator = new RandomGenerator2()
+		{
+			Random random = new Random();
+			
+			@Override
+			public BigDecimal getRandom() 
+			{
+				return new BigDecimal(random.nextDouble());
+			}
+		};
+		return generator;
 	}
 }
