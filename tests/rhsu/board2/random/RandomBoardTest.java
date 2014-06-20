e5e083ae019a8311c87a78fa122dd52c02adda82
rhsu <rhsu123@gmail.com>
2014-06-20 12:54:32 -0400

    added in unit tests

---
+++ tests/rhsu/board2/random/RandomBoardTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rhsu.board2.random;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rhsu
+ */
+public class RandomBoardTest {
+
+    public RandomBoardTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+	@Test
+	public void testRandomGenerator()
+	{
+	}
+}
