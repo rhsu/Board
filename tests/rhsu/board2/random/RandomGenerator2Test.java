133368587814258d4a889fa39af4793bdaf84e8f
rhsu <rhsu123@gmail.com>
2014-07-08 10:52:49 -0400

    Idon't know

--- tests/rhsu/board2/random/RandomGenerator2Test.java
+++ tests/rhsu/board2/random/RandomGenerator2Test.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package rhsu.board2.random;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-/**
- *
- * @author rhsu
- */
 public class RandomGenerator2Test {
 
     public RandomGenerator2Test() {
