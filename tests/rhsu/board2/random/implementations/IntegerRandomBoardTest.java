0503cf5b77d1d3bae8c6e54e8d35f4808defea78
rhsu <rhsu123@gmail.com>
2014-07-08 10:54:40 -0400

    close #210

--- tests/rhsu/board2/random/implementations/IntegerRandomBoardTest.java
+++ tests/rhsu/board2/random/implementations/IntegerRandomBoardTest.java
@@ -1,10 +1,11 @@
 package rhsu.board2.random.implementations;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import rhsu.board2.factory.BoardFactory;
 import rhsu.board2.random.RandomBoardTest;
 
-
+@Ignore
 public class IntegerRandomBoardTest extends RandomBoardTest
 {
     @Before
