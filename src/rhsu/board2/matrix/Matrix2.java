1493e1e1a3e87f828456eb97bd734e0cf9bfbb66
rhsu <rhsu123@gmail.com>
2014-07-17 13:31:38 -0400

    #185

--- src/rhsu/board2/matrix/Matrix2.java
+++ src/rhsu/board2/matrix/Matrix2.java
@@ -1,10 +1,7 @@
 package rhsu.board2.matrix;
 
-/**
- * A extension of the Board Interface to allow mathematical/matrix operations. This extension allows a board to be viewed as a 2 dimensional Matrix
- * @param <T> The type of the matrix object
- * @author rhsu
- */
+import rhsu.compositeBoard.CompositeBoard;
+
 public interface Matrix2<T>
 {	
 	/**
@@ -12,34 +9,34 @@ public interface Matrix2<T>
 	 * @param matrix
 	 * @return the result after the add operation
 	 */
-	public CompositeBoard<T> add(Matrix2<T> matrix);
+	public CompositeBoard<T> add(CompositeBoard<T> matrix);
 	
 	/**
 	 * Performs the subtract operation
 	 * @param matrix
 	 * @return the result after the subtract operation
 	 */
-	public Matrix2<T> subtract(Matrix2<T> matrix);
+	public CompositeBoard<T> subtract(CompositeBoard<T> matrix);
 	
 	/**
 	 * Performs the multiply operation
 	 * @param matrix
 	 * @return the result after the multiply operation
 	 */
-	public Matrix2<T> multiply(Matrix2<T> matrix);
+	public CompositeBoard<T> multiply(CompositeBoard<T> matrix);
 	
 	/**
 	 * Performs the multiply scalar operation
 	 * @param piece the scalar to multiply to the matrix
 	 * @return the result after the multiply scalar operation
 	 */
-	public Matrix2<T> multiply(T piece);
+	public CompositeBoard<T> multiply(T piece);
 	
 	/**
 	 * Performs the inverse operation
 	 * @return the result after the inverse operation
 	 */
-	public Matrix2<T> inverse();
+	public CompositeBoard<T> inverse();
 	
 	/**
 	 * Performs the determinant operation
@@ -47,9 +44,9 @@ public interface Matrix2<T>
 	 */
 	public T determinant();
 	
-	public Matrix2<T> transpose();
+	public CompositeBoard<T> transpose();
 	
-	public Matrix2<T> createSubMatrix(int excluding_row, int excluding_column);
+	public CompositeBoard<T> createSubMatrix(int excluding_row, int excluding_column);
 	
-	public Matrix2<T> cofactor();
+	public CompositeBoard<T> cofactor();
 }
