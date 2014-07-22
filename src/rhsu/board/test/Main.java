ac39f3fb44dea195c2509ee12b4f154dbf6631ed
rhsu <rhsu123@gmail.com>
2014-07-22 15:46:22 -0400

    #219: removed board2

--- src/rhsu/board/test/Main.java
+++ src/rhsu/board/test/Main.java
@@ -1,16 +1,5 @@
 package rhsu.board.test;
 
-import rhsu.compositeBoard.BoardBuilder;
-import rhsu.compositeBoard.CompositeBoard;
-import rhsu.board.*;
-import rhsu.board.basic.implementations.*;
-import rhsu.board.basic.implementations.arithmetic.*;
-import rhsu.board.resources.ResourceRetriever;
-import rhsu.board2.Board2;
-import rhsu.board2.BoardImpl;
-import rhsu.board2.boardCores.IntegerBoard2;
-import rhsu.board2.boardCores.StringBoard2;
-
 public class Main
 {	
     private static final String TEST_RESOURCE = "/rhsu/board/resources/test/";
