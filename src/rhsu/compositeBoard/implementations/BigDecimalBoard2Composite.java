9390f0bb28f9d32ff851b336ecf280cd31f66ba6
rhsu <rhsu123@gmail.com>
2014-07-22 09:54:27 -0400

    reomved unused imports

--- src/rhsu/compositeBoard/implementations/BigDecimalBoard2Composite.java
+++ src/rhsu/compositeBoard/implementations/BigDecimalBoard2Composite.java
@@ -2,11 +2,6 @@ package rhsu.compositeBoard.implementations;
 
 import java.math.BigDecimal;
 
-import rhsu.board.io.BoardIO;
-import rhsu.board2.Board2;
-import rhsu.board2.matrix2.Matrix2;
-import rhsu.board2.random.RandomBoard;
-import rhsu.compositeBoard.MobilityBoard;
 import rhsu.compositeBoard.BoardBuilder;
 import rhsu.compositeBoard.CompositeBoard;
 
