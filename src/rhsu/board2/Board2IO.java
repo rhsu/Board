be5f3b53c20c7134e3de20b7ce001dfd4abc1f00
rhsu <rhsu123@gmail.com>
2014-07-24 17:03:16 -0400

    #230

---
+++ src/rhsu/board2/Board2IO.java
@@ -0,0 +1,32 @@
+package rhsu.board2;
+
+import java.io.BufferedReader;
+import rhsu.board.io.BoardIO;
+
+public class Board2IO implements BoardIO
+{
+	@Override
+	public void populateFromFile(String filename)
+	{
+
+	}
+
+	@Override
+	public void populateFromResource(BufferedReader reader)
+	{
+		
+	}
+
+	@Override
+	public void export(String filename, char delimiter)
+	{
+		
+	}
+	
+	@Override
+	public void initializeFromBaseBoard()
+	{
+		throw new UnsupportedOperationException("Not supported yet."); 
+	}
+	
+}
