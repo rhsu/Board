3d5635d1f16c4451b3c20a14b76f383d6cd61182
rhsu <rhsu123@gmail.com>
2014-08-07 16:01:51 -0400

    185: got abstract matrix 2 working

--- src/rhsu/board2/implementations/BigDecimalBoard2Composite.java
+++ src/rhsu/board2/implementations/BigDecimalBoard2Composite.java
@@ -4,11 +4,70 @@ import java.math.BigDecimal;
 
 import rhsu.board2.BoardBuilder;
 import rhsu.board2.CompositeBoard;
+import rhsu.board2.Matrix2;
 import rhsu.board2.randomGenerators.AbstractRandomGenerator;
 import rhsu.board2.randomGenerators.BigDecimalRandomGenerator;
 
 public class BigDecimalBoard2Composite
 {
+	private class BigDecimalMatrix implements Matrix2
+	{
+		@Override
+		public CompositeBoard add(CompositeBoard matrix)
+		{
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public CompositeBoard subtract(CompositeBoard matrix)
+		{
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public CompositeBoard multiply(CompositeBoard matrix)
+		{
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public CompositeBoard multiply(Object piece)
+		{
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public CompositeBoard inverse()
+		{
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public Object determinant()
+		{
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public CompositeBoard transpose()
+		{
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public CompositeBoard createSubMatrix(int excluding_row, int excluding_column)
+		{
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public CompositeBoard cofactor()
+		{
+			throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		}
+		
+	}
+	
 	static final BigDecimal DEFAULT_VALUE = BigDecimal.ZERO;
 
 	public static CompositeBoard<BigDecimal> createBigDecimalBoard(int horizontalSize, 
