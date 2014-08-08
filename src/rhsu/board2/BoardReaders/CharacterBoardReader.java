042ddca9ed73ad4f9eec5fb129ca56dec773488c
rhsu <rhsu123@gmail.com>
2014-08-08 12:39:37 -0400

    #230: This looks kind of right

--- src/rhsu/board2/BoardReaders/CharacterBoardReader.java
+++ src/rhsu/board2/BoardReaders/CharacterBoardReader.java
@@ -13,14 +13,22 @@ public class CharacterBoardReader extends AbstractBoardIO<Character>
 		for (BoardPiece2<Character>[] row : boardArray)
 		{
 			for (int rowNumber = 0; rowNumber < row.length; rowNumber++) 
-			{ 
+			{
+				String stringValue = this.getBoardInitializer()
+					.getValueAt(rowNumber, columnNumber);
+				
 				row[rowNumber] = new BoardPieceImpl(rowNumber, 
 					columnNumber, 
-					this.getBoardInitializer().getPieceAt(rowNumber, columnNumber));
+					this.convertFromString(stringValue));
 			}
 			columnNumber++;
 		}
 		
 		return boardArray;
-	}	
+	}
+	
+	protected Character convertFromString(String string)
+	{
+		return string.charAt(0);
+	}
 }
