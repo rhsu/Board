f4c8f28a0370d2666a49a24b478020b2c8d75168
rhsu <rhsu123@gmail.com>
2014-09-24 12:59:38 -0400

    backup

---
+++ tests/rhsu/board2/implementations/factories/BoardFactoryClientTest.java
@@ -0,0 +1,44 @@
+package rhsu.board2.implementations.factories;
+
+import org.junit.*;
+
+public class BoardFactoryClientTest
+{
+	private final BoardFactoryClient factoryClient = new BoardFactoryClient();
+	
+	@Test
+	public void testBigDecimalBoard()
+	{
+		
+	}
+	
+	@Test
+	public void testBigIntegerBoard()
+	{
+		
+	}
+	
+	@Test
+	public void testBooleanBoard()
+	{
+		
+	}
+	
+	@Test
+	public void testCharacterBoard()
+	{
+		
+	}
+	
+	@Test
+	public void testDecimalBoard()
+	{
+		
+	}
+	
+	@Test
+	public void testIntegerBoard()
+	{
+		
+	}
+}
