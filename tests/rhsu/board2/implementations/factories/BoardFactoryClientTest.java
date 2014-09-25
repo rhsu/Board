2403327c5f7aea2fae955737fafdc937fcd48690
rhsu <rhsu123@gmail.com>
2014-09-25 14:54:17 -0400

    Created defaultValue Provider

--- tests/rhsu/board2/implementations/factories/BoardFactoryClientTest.java
+++ tests/rhsu/board2/implementations/factories/BoardFactoryClientTest.java
@@ -1,46 +1,72 @@
 package rhsu.board2.implementations.factories;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import org.junit.*;
+import static org.junit.Assert.*;
+import rhsu.board2.CompositeBoard;
 
 public class BoardFactoryClientTest
 {
-	private final BoardFactoryClient factoryClient = new BoardFactoryClient();
+	private final BoardFactoryClient factoryClient;
+	private final int horizontalSize;
+	private final int verticalSize;
+	
+	private BoardFactoryClientTest()
+	{
+		factoryClient = new BoardFactoryClient();
+		horizontalSize = 3;
+		verticalSize = 4;
+	}
+	
 	
 	@Test
 	public void testBigDecimalBoard()
 	{
+		BigDecimal defaultValue = BigDecimal.ZERO;
 		
+		CompositeBoard<BigDecimal> result
+			= factoryClient.GetBigDecimalBoardFactory(BigDecimal.ZERO).createBoard(horizontalSize, verticalSize);
+		assertNotNull(result);
 	}
 	
 	@Test
 	public void testBigIntegerBoard()
 	{
-		
+		CompositeBoard<BigInteger> result
+			= factoryClient.GetBigIntegerBoardFactory().createBoard(horizontalSize, verticalSize);
+		assertNotNull(result);
 	}
 	
 	@Test
 	public void testBooleanBoard()
 	{
-		
+		CompositeBoard<Boolean> result 
+			= factoryClient.GetBooleanBoardFactory().createBoard(horizontalSize, verticalSize);
+		assertNotNull(result);
 	}
 	
 	@Test
 	public void testCharacterBoard()
 	{
-		
+		CompositeBoard<Character> result 
+			= factoryClient.GetCharacterBoardFactory().createBoard(horizontalSize, verticalSize);
+		assertNotNull(result);
 	}
 	
-	
-	
 	@Test
-	public void testDecimalBoard()
+	public void testDoubleBoard()
 	{
-		
+		CompositeBoard<Double> result
+			= factoryClient.GetDoubleBoardFactory().createBoard(horizontalSize, verticalSize);
+		assertNotNull(result);
 	}
 	
 	@Test
 	public void testIntegerBoard()
 	{
-		
+		CompositeBoard<Integer> result
+			= factoryClient.GetIntegerBoardFactory().createBoard(horizontalSize, verticalSize);
+		assertNotNull(result);
 	}
 }
