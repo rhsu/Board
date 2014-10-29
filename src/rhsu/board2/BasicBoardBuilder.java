94b094a7a8c7e5ed641b5e7539bfbe910506a640
rhsu <rhsu123@gmail.com>
2014-10-29 14:28:49 -0400

    #306

--- src/rhsu/board2/BasicBoardBuilder.java
+++ src/rhsu/board2/BasicBoardBuilder.java
@@ -1,76 +1,7 @@
 package rhsu.board2;
 
-import rhsu.board2.mobility.MobilityBoard;
-import rhsu.board2.randomGenerators.RandomGenerator;
-import rhsu.board2.matrices.Matrix2;
-import rhsu.board2.boardIO.Board2IO;
-
-public class BasicBoardBuilder<T> implements BoardBuilder<T>
+public class BasicBoardBuilder<T> extends AbstractBoardBuilder
 {
-	protected Board2IO boardIO = null;
-	protected Matrix2<T> matrix = null;
-	protected MobilityBoard<T> mobilityBoard = null;
-	protected RandomGenerator<T> randomGenerator = null;
-	protected Integer horizontalSize = null;
-	protected Integer verticalSize = null; 
-	protected T defaultValue = null;
-	protected BoardInitializable<T> boardInitializer = null;
-	
-	@Override
-	public BasicBoardBuilder<T> setBoardIO(Board2IO<T> boardIO)
-	{
-		this.boardIO = boardIO;
-		return this;
-	}
-	
-	@Override
-	public BasicBoardBuilder<T> setMatrix(Matrix2<T> matrix)
-	{
-		this.matrix = matrix;
-		return this;
-	}
-	
-	public BasicBoardBuilder<T> setRandomGenerator(RandomGenerator<T> randomGenerator)
-	{
-		this.randomGenerator = randomGenerator;
-		return this;
-	}
-	
-	@Override
-	public BasicBoardBuilder<T> setMobilityBoard(MobilityBoard<T> mobilityBoard)
-	{
-		this.mobilityBoard = mobilityBoard;
-		return this;
-	}
-	
-	@Override
-	public BasicBoardBuilder<T> setHorizontalSize(int horizontalSize)
-	{
-		this.horizontalSize = horizontalSize;
-		return this;
-	}
-	
-	@Override
-	public BasicBoardBuilder<T> setVerticalSize(int verticalSize)
-	{
-		this.verticalSize = verticalSize;
-		return this;
-	}
-	
-	@Override
-	public BasicBoardBuilder<T> setDefaulValue(T defaultValue)
-	{
-		this.defaultValue = defaultValue;
-		return this;
-	}
-	
-	@Override
-	public BasicBoardBuilder<T> setBoardInitializable(BoardInitializable<T> boardInitializer)
-	{
-		this.boardInitializer = boardInitializer;
-		return this;
-	}
-	
 	@Override
 	public Board2<T> createBoard()
 	{		
--- src/rhsu/board2/BoardBuilder.java
+++ src/rhsu/board2/BoardBuilder.java
@@ -7,13 +7,13 @@ import rhsu.board2.randomGenerators.RandomGenerator;
 
 public interface BoardBuilder<T>
 {
-	public BasicBoardBuilder<T> setBoardIO(Board2IO<T> boardIO);
-	public BasicBoardBuilder<T> setMatrix(Matrix2<T> matrix);
-	public BasicBoardBuilder<T> setRandomGenerator(RandomGenerator<T> randomGenerator);
-	public BasicBoardBuilder<T> setMobilityBoard(MobilityBoard<T> mobilityBoard);
-	public BasicBoardBuilder<T> setHorizontalSize(int horizontalSize);	
-	public BasicBoardBuilder<T> setVerticalSize(int verticalSize);
-	public BasicBoardBuilder<T> setDefaulValue(T defaultValue);
-	public BasicBoardBuilder<T> setBoardInitializable(BoardInitializable<T> boardInitializer);
+	public BoardBuilder<T> setBoardIO(Board2IO<T> boardIO);
+	public BoardBuilder<T> setMatrix(Matrix2<T> matrix);
+	public BoardBuilder<T> setRandomGenerator(RandomGenerator<T> randomGenerator);
+	public BoardBuilder<T> setMobilityBoard(MobilityBoard<T> mobilityBoard);
+	public BoardBuilder<T> setHorizontalSize(int horizontalSize);	
+	public BoardBuilder<T> setVerticalSize(int verticalSize);
+	public BoardBuilder<T> setDefaulValue(T defaultValue);
+	public BoardBuilder<T> setBoardInitializable(BoardInitializable<T> boardInitializer);
 	public Board2<T> createBoard();
 }
